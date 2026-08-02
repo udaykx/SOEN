0224c1bc169b9ef3aab4ac6f31deead819820f3a
uday <udaykumar2600@gmail.com>
2026-08-03 01:17:24 +0530

    implement project socket

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,9 +4,8 @@ import './index.css'
 import 'remixicon/fonts/remixicon.css'
 import App from './App.jsx'
 
-
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+  <App />
+
 )
