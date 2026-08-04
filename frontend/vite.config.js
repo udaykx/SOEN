c243115d686f5313a696bd06dbe94542d6962140
uday <udaykumar2600@gmail.com>
2026-08-05 01:52:21 +0530

    complete

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,11 +3,18 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server:{
-    headers:{
-      "Cross-Origin-Embedder-Policy":"require-corp",
-      "Cross-Origin-Opner-Policy":"same-origin"
+  plugins: [ react() ],
+  server: {
+    headers: {
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Opener-Policy": "same-origin"
+    },
+    proxy: {
+      '/cdn': {
+        target: 'https://unpkg.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/cdn/, '')
+      }
     }
   }
 })
