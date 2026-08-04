860c309876ec60a7b3c318df3046a0cd7bb8ed32
uday <udaykumar2600@gmail.com>
2026-08-04 23:17:28 +0530

    AI services create

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import { Server } from 'socket.io';
 import jwt from 'jsonwebtoken';
 import mongoose from 'mongoose';
 import projectModel from './models/project.model.js';
-// import { generateResult } from './services/ai.service.js';
+import { generateResult } from './services/ai.service.js';
 
 const port = process.env.PORT || 3000;
 
